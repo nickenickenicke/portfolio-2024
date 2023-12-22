4b8def351a0f285c8bd6e47e67a18c904ad9fc07
Nicke Bergman <nickenickebergman@gmail.com>
2023-12-22 14:44:33 +0100

    :sparkles: Added ability to switch languages

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -28,3 +28,45 @@ const toggleModal = () => {
   modalContainer?.classList.toggle("display-none");
   modalContainer?.classList.toggle("modal-container");
 };
+
+/* FIND ALL SPANS AND CHECK FOR lang ATTRIBUTE */
+let spanElements: HTMLSpanElement[] = Array.from(
+  document.getElementsByTagName("span")
+) as HTMLSpanElement[];
+
+let spanSv: HTMLSpanElement[] = [];
+let spanEn: HTMLSpanElement[] = [];
+
+spanElements.forEach((element) => {
+  if (element.lang === "sv") spanSv.push(element);
+  if (element.lang === "en") spanEn.push(element);
+});
+
+/* BOOL FOR IF LANGUAGE IS SWEDISH OR NOT (false = English) */
+let isLangSv: boolean = true;
+
+/* BUTTON TO SWITCH LANGUAGES */
+const btnLanguage = document.getElementById("language-btn");
+btnLanguage?.addEventListener("click", () => {
+  if (isLangSv) {
+    spanEn.forEach((element) => {
+      element.classList.toggle("display-none");
+    });
+    spanSv.forEach((element) => {
+      element.classList.toggle("display-none");
+    });
+    btnLanguage.innerText = "Svenska";
+  }
+  if (!isLangSv) {
+    spanEn.forEach((element) => {
+      element.classList.toggle("display-none");
+    });
+    spanSv.forEach((element) => {
+      element.classList.toggle("display-none");
+    });
+    btnLanguage.innerText = "English";
+  }
+  isLangSv = !isLangSv;
+  //Save to LS for next visit
+  localStorage.setItem("IsSelectedLanguageSv", JSON.stringify(isLangSv));
+});
