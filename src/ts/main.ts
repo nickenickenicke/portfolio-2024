0c902c95427ea579ea865539d1901c8205710fa8
Nicke Bergman <nickenickebergman@gmail.com>
2023-12-22 15:12:24 +0100

    :art: More language select

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -14,21 +14,6 @@ modalContainer?.addEventListener("click", (e) => {
   }
 });
 
-/* PREPARE FOR LANGUAGE SELECT */
-const btnInitalLangSv = document.getElementById("btn-initial-lang-sv");
-const btnInitalLangEn = document.getElementById("btn-initial-lang-en");
-btnInitalLangSv?.addEventListener("click", () => {
-  toggleModal();
-});
-btnInitalLangEn?.addEventListener("click", () => {
-  toggleModal();
-});
-
-const toggleModal = () => {
-  modalContainer?.classList.toggle("display-none");
-  modalContainer?.classList.toggle("modal-container");
-};
-
 /* FIND ALL SPANS AND CHECK FOR lang ATTRIBUTE */
 let spanElements: HTMLSpanElement[] = Array.from(
   document.getElementsByTagName("span")
@@ -48,25 +33,44 @@ let isLangSv: boolean = true;
 /* BUTTON TO SWITCH LANGUAGES */
 const btnLanguage = document.getElementById("language-btn");
 btnLanguage?.addEventListener("click", () => {
-  if (isLangSv) {
-    spanEn.forEach((element) => {
-      element.classList.toggle("display-none");
-    });
-    spanSv.forEach((element) => {
-      element.classList.toggle("display-none");
-    });
-    btnLanguage.innerText = "Svenska";
-  }
-  if (!isLangSv) {
-    spanEn.forEach((element) => {
-      element.classList.toggle("display-none");
-    });
-    spanSv.forEach((element) => {
-      element.classList.toggle("display-none");
-    });
-    btnLanguage.innerText = "English";
-  }
+  switchLanguages();
   isLangSv = !isLangSv;
-  //Save to LS for next visit
-  localStorage.setItem("IsSelectedLanguageSv", JSON.stringify(isLangSv));
+  saveLanguageToLs();
 });
+
+/* PREPARE FOR LANGUAGE SELECT */
+const btnInitalLangSv = document.getElementById("btn-initial-lang-sv");
+const btnInitalLangEn = document.getElementById("btn-initial-lang-en");
+btnInitalLangSv?.addEventListener("click", () => {
+  //Default language is Swedish
+  saveLanguageToLs();
+  toggleModal();
+});
+btnInitalLangEn?.addEventListener("click", () => {
+  switchLanguages();
+  isLangSv = false;
+  saveLanguageToLs();
+  toggleModal();
+});
+
+const toggleModal = () => {
+  modalContainer?.classList.toggle("display-none");
+  modalContainer?.classList.toggle("modal-container");
+};
+
+/* FUNCTION TO SWITCH LANGUAGE TEXTS */
+const switchLanguages = () => {
+  spanEn.forEach((element) => {
+    element.classList.toggle("display-none");
+  });
+  spanSv.forEach((element) => {
+    element.classList.toggle("display-none");
+  });
+  if (btnLanguage) btnLanguage.innerHTML = "Svenska";
+  if (btnLanguage && !isLangSv) btnLanguage.innerHTML = "English";
+};
+
+/* FUNCTION TO SAVE LAGUAGE TO LS FOR FUTURE VISITS */
+const saveLanguageToLs = () => {
+  localStorage.setItem("IsSelectedLanguageSv", JSON.stringify(isLangSv));
+};
